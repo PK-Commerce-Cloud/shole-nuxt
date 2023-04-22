dab6eb8b8e76476ddf1810ace7a445773624a6a9
Fermin Villanueva <FerVillanuev4s@gmail.com>
2023-04-22 15:15:09 -0700

    Fix guard

--- middleware/session.global.ts
+++ middleware/session.global.ts
@@ -1,6 +1,6 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
   const cookie = useCookie("session");
   if (from.name !== 'loading' && to.name !== "loading" && !cookie.value?.session.access_token) {
-    return navigateTo("loading");
+    return navigateTo("/loading");
   }
 });
